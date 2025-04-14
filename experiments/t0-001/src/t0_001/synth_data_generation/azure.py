33ed97fcabba873e7c4d5ab7d6841ccaecda75f2
Rosie Wood <rwood@turing.ac.uk>
2025-04-14 11:39:31 +0100

    update to use the same env variables as other code

--- experiments/t0-001/src/t0_001/synth_data_generation/azure.py
+++ experiments/t0-001/src/t0_001/synth_data_generation/azure.py
@@ -5,26 +5,58 @@ from azure.ai.inference import ChatCompletionsClient
 from azure.ai.inference.models import UserMessage
 from azure.core.credentials import AzureKeyCredential
 
+AZURE_OPENAI_ENDPOINTS = {
+    "gpt-4o",
+    "o3-mini",
+    "o1",
+}
 
-def set_up_azure_client():
+
+def get_azure_openai_endpoint(model: str):
+    """Get the Azure OpenAI endpoint from the environment variables.
+    The endpoint is stored in the environment variable AZURE_OPENAI_ENDPOINT_{model}.
+
+    Parameters
+    ----------
+    model : str
+        The model to use. Must be one of the following: gpt-4o, o3-mini, o1.
+    """
+    logging.getLogger(__name__)
+
+    try:
+        endpoint = os.environ[f"AZURE_OPENAI_ENDPOINT_{model.lower()}"]
+    except KeyError:
+        raise KeyError(
+            f"Please set the AZURE_OPENAI_ENDPOINT_{model.lower()} environment variable."
+        )
+
+    logging.info(f"Using Azure OpenAI endpoint: {endpoint}")
+    return endpoint
+
+
+def get_azure_openai_key():
+    """Get the Azure OpenAI key from the environment variables."""
+    try:
+        return os.environ["AZURE_OPENAI_KEY"]
+    except KeyError:
+        raise KeyError("Please set the AZURE_OPENAI_KEY environment variable.")
+
+
+def set_up_azure_client(endpoint: str, key: str) -> ChatCompletionsClient:
     """Set up the Azure OpenAI client.
+
+    Parameters
+    ----------
+    endpoint : str
+        The Azure OpenAI endpoint.
+    key : str
+        The Azure OpenAI key.
 
     Returns
     -------
     ChatCompletionsClient
         The Azure OpenAI client.
     """
-    # set up the environment
-    try:
-        endpoint = os.environ["AZURE_OPENAI_ENDPOINT"]
-    except KeyError:
-        raise KeyError("Please set the AZURE_OPENAI_ENDPOINT environment variable.")
-
-    try:
-        key = os.environ["AZURE_OPENAI_KEY"]
-    except KeyError:
-        raise KeyError("Please set the AZURE_OPENAI_KEY environment variable.")
-
     # set up the client
     return ChatCompletionsClient(
         endpoint=endpoint,
