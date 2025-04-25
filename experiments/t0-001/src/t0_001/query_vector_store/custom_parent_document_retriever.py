98f6211a84696b17bfb8b19f001a3e66e62af2bb
rchan <rchan@turing.ac.uk>
2025-04-25 11:40:34 +0100

    add async methods for retrieval

--- experiments/t0-001/src/t0_001/query_vector_store/custom_parent_document_retriever.py
+++ experiments/t0-001/src/t0_001/query_vector_store/custom_parent_document_retriever.py
@@ -4,7 +4,10 @@ from typing import Any, List, Optional
 
 from langchain.retrievers import ParentDocumentRetriever
 from langchain.retrievers.multi_vector import SearchType
-from langchain_core.callbacks import CallbackManagerForRetrieverRun
+from langchain_core.callbacks import (
+    AsyncCallbackManagerForRetrieverRun,
+    CallbackManagerForRetrieverRun,
+)
 from langchain_core.documents import Document
 from tqdm import tqdm
 
@@ -56,6 +59,57 @@ class CustomParentDocumentRetriever(ParentDocumentRetriever):
         docs = []
         for _id, sub_docs in id_to_doc.items():
             docstore_docs = self.docstore.mget([_id])
+            if docstore_docs:
+                if doc := docstore_docs[0]:
+                    doc.metadata["sub_docs"] = sub_docs
+                    docs.append(doc)
+
+        return docs
+
+    async def _aget_relevant_documents(
+        self, query: str, *, run_manager: AsyncCallbackManagerForRetrieverRun
+    ) -> List[Document]:
+        """Asynchronously get documents relevant to a query.
+        Args:
+            query: String to find relevant documents for
+            run_manager: The callbacks handler to use
+        Returns:
+            List of relevant documents
+        """
+        if self.search_type == SearchType.mmr:
+            sub_docs = await self.vectorstore.amax_marginal_relevance_search(
+                query, **self.search_kwargs
+            )
+
+            # We do this to maintain the order of the ids that are returned
+            ids = []
+            for d in sub_docs:
+                if self.id_key in d.metadata and d.metadata[self.id_key] not in ids:
+                    ids.append(d.metadata[self.id_key])
+            docs = await self.docstore.amget(ids)
+
+            return [d for d in docs if d is not None]
+        elif self.search_type == SearchType.similarity_score_threshold:
+            sub_docs = await self.vectorstore.asimilarity_search_with_relevance_scores(
+                query, **self.search_kwargs
+            )
+        else:
+            sub_docs = await self.vectorstore.asimilarity_search_with_score(
+                query, **self.search_kwargs
+            )
+
+        # Map doc_ids to list of sub-documents, adding scores to metadata
+        id_to_doc = defaultdict(list)
+        for doc, score in sub_docs:
+            doc_id = doc.metadata.get("doc_id")
+            if doc_id:
+                doc.metadata["score"] = float(score)
+                id_to_doc[doc_id].append(doc)
+
+        # Fetch documents corresponding to doc_ids, retaining sub_docs in metadata
+        docs = []
+        for _id, sub_docs in id_to_doc.items():
+            docstore_docs = await self.docstore.amget([_id])
             if docstore_docs:
                 if doc := docstore_docs[0]:
                     doc.metadata["sub_docs"] = sub_docs
