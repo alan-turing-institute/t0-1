5b1ad0e2db986247da125fafa3022bdd7535fdb3
Penelope Yong <penelopeysm@gmail.com>
2025-05-02 14:47:59 +0100

    Add option for multiple conversation threads

--- web/src/lib/Header.svelte
+++ web/src/lib/Header.svelte
@@ -1,15 +1,43 @@
 <script lang="ts">
     interface Props {
+        allIds: Array<string>;
+        currentId: string;
+        changeId: (id: string) => void;
+        newConversation: () => void;
         darkMode: boolean;
         toggleTheme: () => void;
     }
 
-    let { darkMode, toggleTheme }: Props = $props();
+    let {
+        allIds,
+        currentId,
+        changeId,
+        newConversation,
+        darkMode,
+        toggleTheme,
+    }: Props = $props();
+
+    function handleSelectChange(event: Event) {
+        const target = event.target as HTMLSelectElement;
+        if (target.value === "new") {
+            newConversation();
+        } else {
+            changeId(target.value);
+        }
+    }
 </script>
 
 <div class="header">
     <h1>t0 online (but really gpt-4o)</h1>
-    <button onclick={(_e) => toggleTheme()}>switch to {darkMode ? "light" : "dark"} mode</button>
+    <select bind:value={currentId} onchange={handleSelectChange}>
+        {#each allIds as id}
+            <option value={id}>{id}</option>
+        {/each}
+        <option value="new">new conversation</option>
+    </select>
+    <button onclick={(_e) => toggleTheme()}
+        >switch to {darkMode ? "light" : "dark"} mode</button
+    >
 </div>
 
 <style>
