aee0157bfd3fecfa9ac9bce150bed35537efbc54
Ryan Chan <rchan@turing.ac.uk>
2025-05-12 09:45:37 +0000

    qwen3 eval scripts

---
+++ experiments/t0-001/scripts/v4_summarised/k5/evaluate_rag_qwen3_32B_k5_summarised.sh
@@ -0,0 +1,11 @@
+uv run t0-001 evaluate-rag ./data/synthetic_queries/5147cd8_gpt-4o_1000_synthetic_queries.jsonl \
+--k 5 \
+--db-choice chroma \
+--llm-provider openai \
+--llm-model-name Qwen/Qwen3-32B \
+--prompt-template-path ./templates/rag_evaluation_prompt.txt \
+--system-prompt-path ./templates/rag_evaluation_system_prompt.txt \
+--output-file ./evaluate-rag-qwen3-32b-k5-chroma.jsonl \
+--conditions-file ./data/nhs-conditions/v4/qwen_summarised_conditions.jsonl \
+--persist-directory ./v4-summarised-db \
+--local-file-store ./v4-summarised-lfs
