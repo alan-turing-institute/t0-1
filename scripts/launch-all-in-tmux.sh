9d38c80dcf022aba66c1b8ea9ca824e670d09d77
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2025-09-25 14:01:22 +0000

    debug tmux launch script

--- scripts/launch-all-in-tmux.sh
+++ scripts/launch-all-in-tmux.sh
@@ -4,6 +4,12 @@ echo "REPO_ROOT_DIR is: $REPO_ROOT_DIR"
 if ! tmux has-session -t t0-1 2>/dev/null; then
     tmux new-session -d -s t0-1
 
+    # Common setup for all panes
+    # tmux send-keys "cd $REPO_ROOT_DIR" C-m
+    # tmux send-keys 'source .env' C-m
+    # tmux send-keys 'source .venv/bin/activate' C-m
+
+
     # Create three panes and run the commands in each pane
     PANE0=$(tmux display-message -p '#{pane_id}')
     tmux split-window -h
@@ -15,15 +21,18 @@ if ! tmux has-session -t t0-1 2>/dev/null; then
     tmux select-pane -t $PANE0
     tmux send-keys "cd $REPO_ROOT_DIR" C-m
     tmux send-keys 'source .env' C-m
-    tmux send-keys 'ls -l ./scripts/serve_rag_conversational.sh' C-m
+    tmux send-keys 'source .venv/bin/activate' C-m
+    tmux send-keys 'source ./scripts/serve_rag_conversational.sh' C-m
     tmux select-pane -t $PANE1
     tmux send-keys "cd $REPO_ROOT_DIR" C-m
     tmux send-keys 'source .env' C-m
-    tmux send-keys 'ls -l ./scripts/serve_t0_1.sh' C-m
+    tmux send-keys 'source .venv/bin/activate' C-m
+    tmux send-keys 'source ./scripts/serve_t0_1.sh' C-m
     tmux select-pane -t $PANE2
     tmux send-keys "cd $REPO_ROOT_DIR" C-m
     tmux send-keys 'source .env' C-m
-    tmux send-keys 'ls -l ./scripts/serve_qwen_with_tools.sh' C-m
+    tmux send-keys 'source .venv/bin/activate' C-m
+    tmux send-keys 'source ./scripts/serve_qwen_with_tools.sh' C-m
 fi
 
 # Finally, attach to the tmux session
