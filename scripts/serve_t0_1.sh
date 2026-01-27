d98afc357a3e5b5ebc08c63b0e59804f1666dffd
Rosie Wood <rwood@turing.ac.uk>
2026-01-27 09:49:58 +0000

    update vllm scripts

--- scripts/serve_t0_1.sh
+++ scripts/serve_t0_1.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 vllm serve TomasLaz/t0-1.1-k5-32B \
+CUDA_VISIBLE_DEVICES=0 vllm serve TomasLaz/t0-1.1-k5-32B \
 --seed 42 \
 --generation-config vllm \
 --port 8010 \
 --tensor-parallel-size 4 \
 --max-model-len 131072 \
---rope-scaling '{"factor":4, "original_max_position_embeddings": 32768, "rope_type":"yarn"}'
+--hf-overrides '{"rope-scaling": {"factor":4, "original_max_position_embeddings": 32768, "rope_type":"yarn"}}'
