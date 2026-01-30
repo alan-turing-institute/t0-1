abb2c1eb3fbe36258f9ff16588d336433979fde9
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2026-01-30 11:09:49 +0000

    Deploys t0 software without data

--- deployment/delete_gpu_resource_group.sh
+++ deployment/delete_gpu_resource_group.sh
@@ -1,5 +1,5 @@
 # Define Resource Group name
-RESOURCE_GROUP="t0-dev-deploy-rg2"
+RESOURCE_GROUP="t0-dev-deploy-gpus"
 
 # az vm delete --verbose -g $RESOURCE_GROUP -n $ANSIBLE_CONTROLLER_VM_NAME
 az group delete --name  $RESOURCE_GROUP
