95c7be348c94e5d283376c8e8bd81575d9705182
Rosie Wood <rwood@turing.ac.uk>
2026-01-30 10:29:11 +0000

    add scripts for running gemma models on isambard-ai

--- scripts/serve_qwen_with_tools.sh
+++ scripts/serve_qwen_with_tools.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 vllm serve Qwen/Qwen2.5-32B-Instruct \
+CUDA_VISIBLE_DEVICES=0,1 vllm serve Qwen/Qwen2.5-32B-Instruct \
 --seed 42 \
 --generation-config vllm \
 --port 8020 \
---tensor-parallel-size 4 \
+--tensor-parallel-size 2 \
 --max-model-len 131072 \
 --rope-scaling '{"factor":4, "original_max_position_embeddings": 32768, "rope_type":"yarn"}' \
 --enable-auto-tool-choice \
