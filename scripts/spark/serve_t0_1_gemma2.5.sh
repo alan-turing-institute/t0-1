fa5860aca37f44f55c0aa25aa212c1e489995fa7
Rosie <rwood@turing.ac.uk>
2026-02-04 11:11:49 +0000

    fix spark config in serve scripts

--- scripts/spark/serve_t0_1_gemma2.5.sh
+++ scripts/spark/serve_t0_1_gemma2.5.sh
@@ -1,9 +1,10 @@
-CUDA_VISIBLE_DEVICES=2,3 vllm serve TomasLaz/t0-2.5-gemma-3-4b-it \
+CUDA_VISIBLE_DEVICES=0 vllm serve TomasLaz/t0-2.5-gemma-3-4b-it \
 --tokenizer google/gemma-3-4b-it \
 --seed 42 \
 --generation-config vllm \
 --port 8010 \
---tensor-parallel-size 2 \
+--tensor-parallel-size 1 \
 --max-model-len 131072 \
 --trust-remote-code \
---rope-scaling '{"factor":4, "original_max_position_embeddings": 32768, "rope_type":"yarn"}'
+--hf-overrides '{"rope-scaling": {"factor":4, "original_max_position_embeddings": 32768, "rope_type":"yarn"}}' \
+--gpu-memory-utilization 0.45
