45564bd8adb0eedd2eaa45f2e89e5860d194c0af
Rosie <rwood@turing.ac.uk>
2026-02-04 11:11:49 +0000

    fix spark config in serve scripts

--- scripts/spark/serve_qwen_with_tools.sh
+++ scripts/spark/serve_qwen_with_tools.sh
@@ -1,9 +1,10 @@
-CUDA_VISIBLE_DEVICES=0,1 vllm serve Qwen/Qwen2.5-32B-Instruct \
+CUDA_VISIBLE_DEVICES=0 vllm serve Qwen/Qwen2.5-32B-Instruct \
 --seed 42 \
 --generation-config vllm \
 --port 8020 \
---tensor-parallel-size 2 \
+--tensor-parallel-size 1 \
 --max-model-len 131072 \
---rope-scaling '{"factor":4, "original_max_position_embeddings": 32768, "rope_type":"yarn"}' \
+--hf-overrides '{"rope-scaling": {"factor":4, "original_max_position_embeddings": 32768, "rope_type":"yarn"}}' \
 --enable-auto-tool-choice \
---tool-call-parser hermes
+--tool-call-parser hermes \
+--gpu-memory-utilization 0.45
