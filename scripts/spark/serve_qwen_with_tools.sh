59cf5c8c8fc1fccc3a8203c4b27186fe72d9b2a6
Rosie <rwood@turing.ac.uk>
2026-02-04 10:33:38 +0000

    add scripts for running on spark

---
+++ scripts/spark/serve_qwen_with_tools.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=0,1 vllm serve Qwen/Qwen2.5-32B-Instruct \
+--seed 42 \
+--generation-config vllm \
+--port 8020 \
+--tensor-parallel-size 2 \
+--max-model-len 131072 \
+--rope-scaling '{"factor":4, "original_max_position_embeddings": 32768, "rope_type":"yarn"}' \
+--enable-auto-tool-choice \
+--tool-call-parser hermes
---
+++ scripts/spark/serve_t0_1_t0-2.5-gemma-3-270m-it.sh
@@ -0,0 +1,10 @@
+CUDA_VISIBLE_DEVICES=0 vllm serve TomasLaz/t0-2.5-gemma-3-270m-it \
+--tokenizer google/gemma-3-4b-it \
+--seed 42 \
+--generation-config vllm \
+--port 8010 \
+--tensor-parallel-size 1 \
+--max-model-len 131072 \
+--trust-remote-code \
+--hf-overrides '{"rope-scaling": {"factor":4, "original_max_position_embeddings": 32768, "rope_type":"yarn"}}' \
+--gpu-memory-utilization 0.45
