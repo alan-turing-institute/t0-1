715d76814f3a528daf5232400577a98da53942db
Rosie <rwood@turing.ac.uk>
2026-02-11 11:51:12 +0000

    add serve-gpt-oss script

---
+++ scripts/spark/serve_gpt-oss_with_tools.sh
@@ -0,0 +1,10 @@
+vllm serve openai/gpt-oss-20b \
+--seed 42 \
+--generation-config vllm \
+--port 8020 \
+--tensor-parallel-size 1 \
+--max-model-len 131072 \
+--hf-overrides '{"rope-scaling": {"factor":4, "original_max_position_embeddings": 32768, "rope_type":"yarn"}}' \
+--enable-auto-tool-choice \
+--tool-call-parser openai \
+--gpu-memory-utilization 0.6
