d29e57efbb4ee53d07f73e437426a5ac04481b3d
t0 User <t0@myVM.2ky52epgl2uuhegplxn2ejteya.zx.internal.cloudapp.net>
2026-02-11 12:21:31 +0000

    all changed files from debug session

--- scripts/isambard-ai/serve_qwen_with_tools.sh
+++ scripts/isambard-ai/serve_qwen_with_tools.sh
@@ -5,6 +5,6 @@ vllm serve Qwen/Qwen2.5-32B-Instruct \
 --port 8020 \
 --tensor-parallel-size 2 \
 --max-model-len 131072 \
---hf-overrides '{"rope_scaling":{"factor":4, "original_max_position_embeddings": 32768, "rope_type":"yarn"}}' \
+--hf-overrides '{"rope-scaling": {"factor":4, "original_max_position_embeddings": 32768, "rope_type":"yarn"}}' \
 --enable-auto-tool-choice \
 --tool-call-parser hermes
