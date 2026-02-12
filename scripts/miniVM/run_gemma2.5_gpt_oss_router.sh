792fc129b43c02e5ee0ccdcc516b79f5bc76ed39
Ubuntu <rwood@miniVM.olgi5q2ajlme3gbrohjownlu0f.zx.internal.cloudapp.net>
2026-02-12 11:21:06 +0000

    add miniVM scripts

---
+++ scripts/miniVM/run_gemma2.5_gpt_oss_router.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# vim: et:ts=4:sts=4:sw=4
+
+# source venv
+source .venv/bin/activate
+echo $(which python)
+
+CUDA_VISIBLE_DEVICES=0 ./scripts/miniVM/serve_gpt-oss_with_tools.sh > serve_gpt-oss_with_tools_out.log 2>&1 &
+# Wait for the REST API to be available
+until curl -s http://localhost:8020/v1/models >/dev/null 2>&1; do
+    sleep 20
+    echo "Waiting for vLLM to start..."
+done
+
+CUDA_VISIBLE_DEVICES=1 ./scripts/miniVM/serve_t0.sh TomasLaz/t0-2.5-gemma-3-4b-it > serve_t0_1_gemma2.5_out.log 2>&1 &
+# Wait for the REST API to be available
+until curl -s http://localhost:8010/v1/models >/dev/null 2>&1; do
+    sleep 20
+    echo "Waiting for vLLM to start..."
+done
+
+# t0-1 evaluate-rag ./data/synthetic_queries/5147cd8_gpt-4o_1000_synthetic_queries.jsonl --k 5 --db-choice chroma --llm-provider openai_completion --llm-model-name TomasLaz/t0-2.5-gemma-3-4b-it --budget-forcing --budget-forcing-kwargs '{"max_tokens_thinking": 256, "num_stop_skips": 3}' --extra-body '{"max_tokens": 256}' --prompt-template-path ./templates/rag_evaluation_prompt_deepseek_r1.txt --system-prompt-path ./templates/rag_evaluation_system_prompt_deepseek_r1.txt --output-file ./evaluate-rag-t0-2.5-gpt-oss-router-k5-32B-thinking256-k5-chroma.jsonl --conditions-file ./data/nhs-conditions/v4/qwen_summarised_conditions.jsonl --persist-directory ./v4-summarised-db --local-file-store ./v4-summarised-lfs
+
---
+++ scripts/miniVM/serve_gpt-oss_with_tools.sh
@@ -0,0 +1,9 @@
+vllm serve openai/gpt-oss-20b \
+--seed 42 \
+--generation-config vllm \
+--port 8020 \
+--tensor-parallel-size 1 \
+--max-model-len 131072 \
+--hf-overrides '{"rope_scaling":{"factor":4, "original_max_position_embeddings": 32768, "rope_type":"yarn"}}' \
+--enable-auto-tool-choice \
+--tool-call-parser openai \
